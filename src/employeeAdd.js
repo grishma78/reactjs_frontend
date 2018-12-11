e0188f653156eb0cbc85236336d5760eece36dca
grishma78 <nairgrishma781@gmail.com>
2018-12-11 14:01:03 +0530

    second commit

--- src/employeeAdd.js
+++ src/employeeAdd.js
@@ -1,89 +1,115 @@
-
 import React, { Component } from "react";
-import axios from 'axios';
-//import ReactDOM from 'react-dom';
-import querystring from 'querystring';
-//import {
-//withRouter
-//} from 'react-router-dom'
+import axios from "axios";
 
-//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-//import App from './App';
+import querystring from "querystring";
 
 export default class employeeAdd extends Component {
-
-  constructor(props) {//Intialize state and bind functions
+  constructor(props) {
+    //Intialize state and bind functions
 
     super(props);
     this.state = {
-      Name: '',
-      Design: '',
-      Salary: '',
-
+      Name: "",
+      Design: "",
+      Salary: ""
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);//bind function in constructor
+    this.handleSubmit = this.handleSubmit.bind(this); //bind function in constructor
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeD = this.handleChangeD.bind(this);
     this.handleChangeS = this.handleChangeS.bind(this);
   }
 
-
-  handleChange(event) { //set state for Name
+  handleChange(event) {
+    //set state for Name
     this.setState({
-      Name: event.target.value,
+      Name: event.target.value
     });
   }
 
-  handleChangeD(event) {  //set state for designation
+  handleChangeD(event) {
+    //set state for designation
     this.setState({
-      Design: event.target.value,
+      Design: event.target.value
     });
   }
 
-  handleChangeS(event) {   //set state for salaary
+  handleChangeS(event) {
+    //set state for salaary
     this.setState({
-      Salary: event.target.value,
+      Salary: event.target.value
     });
   }
 
-
-  handleSubmit(event) { //function after submit
+  handleSubmit(event) {
+    //function after submit
     event.preventDefault();
     var data = {
-      'name': this.state.Name,
-      'designation': this.state.Design,
-      'salary': this.state.Salary
-    }
+      name: this.state.Name,
+      designation: this.state.Design,
+      salary: this.state.Salary
+    };
 
-    axios.post("/emp", querystring.stringify(data)) //posting data using axios in server
+    axios
+      .post("/emp", querystring.stringify(data)) //posting data using axios in server
       .then(res => {
         console.log(res);
       })
       .catch(err => {
         console.log(err);
-      })
+      });
 
     window.location.assign("/");
     //this.props.history.push('/');
   }
 
-
   render() {
-
-
     return (
       <div className="content">
-        <form className="form" onSubmit={this.handleSubmit} id="formContact" method="POST">
+        <form
+          className="form"
+          onSubmit={this.handleSubmit}
+          id="formContact"
+          method="POST"
+        >
+          <input
+            id="name"
+            type="name"
+            name="Name"
+            pattern="[A-Z][a-z]*[[\s][A-Z][a-z]*]*"
+            title="First letter capital,Only Alphabets"
+            placeholder="Employee Name"
+            onChange={this.handleChange}
+            required
+          />
+          <br />
 
-          <input id="name" type="name" name="Name" pattern='[A-Z][a-z]*[[\s][A-Z][a-z]*]*' title="First letter capital,Only Alphabets" placeholder="Employee Name" onChange={this.handleChange} required />
-          <br></br>
-
-          <input name="Designation" placeholder="Employee designation" pattern='[A-Z][a-z]*[[\s][A-Z][a-z]*]*' title="First letter capital,Only Alphabets" onChange={this.handleChangeD} required /><br></br>
-          <input name="Salary" placeholder="Employee salary" pattern='((\d+)((\.\d{1,2})?))$' title="Only numbers,2 digits after decimal" onChange={this.handleChangeS} required /><br></br>
-          <input type="submit" value="Submit" className="btn--cta" id="btn-submit" />
-
+          <input
+            name="Designation"
+            placeholder="Employee designation"
+            pattern="[A-Z][a-z]*[[\s][A-Z][a-z]*]*"
+            title="First letter capital,Only Alphabets"
+            onChange={this.handleChangeD}
+            required
+          />
+          <br />
+          <input
+            name="Salary"
+            placeholder="Employee salary"
+            pattern="((\d+)((\.\d{1,2})?))$"
+            title="Only numbers,2 digits after decimal"
+            onChange={this.handleChangeS}
+            required
+          />
+          <br />
+          <input
+            type="submit"
+            value="Submit"
+            className="btn--cta"
+            id="btn-submit"
+          />
         </form>
-      </div>)
+      </div>
+    );
   }
 }
